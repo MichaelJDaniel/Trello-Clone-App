1313dcfe63829fae653dab7c7c67b960a7edd476
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-29 13:47:22 -0700

    add styles to boards

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,41 @@
 class ListsController < ApplicationController
-  def index
-    @lists= List.new
-  end
-
-  def show
-    @cards = @list.card.all
-    @board = Board.find(params)[:board_id]
-  end
+  before_action :set_board, only: [:new, :create, :edit, :update]
+  before_action :set_list, only: [:edit, :update, :destroy]
 
   def new
-    @list = List.new
+    @lists = @board.lists.new
+  end
+
+  def create
+    @board.lists.create_list(list_params, @board.id)
+    redirect_to @board
   end
 
   def edit
-
   end
 
-  def create 
-    @list = List.new[list_params]
+  def update
+    List.update_list(@list.id, list_params)
+    redirect_to @board
   end
+
+  def destroy
+    @list.destroy
+    redirect_to board_path(@list.board_id)
+  end
+
+  private
+    def set_list
+      @list = List.single_list(params[:id])
+    end
+
+    def set_board
+      @board = Board.single_board(params[:board_id])
+    end
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
+
 
 end
