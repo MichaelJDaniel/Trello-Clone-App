4d8becf7eb4b84587b45795bfc940c9e46d70b92
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-30 09:30:30 -0700

    add more styles to login

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,42 @@
 class CardsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  before_action :set_list
+  before_action :set_card, only: [:edit, :update, :destroy]
 
   def new
+    @cards = @list.cards.new
+  end
+
+  def create
+    @list.cards.create_card(cards_params, @list.id)
+    redirect_to board_path(@list.board_id)
   end
 
   def edit
   end
+
+  def update
+    Task.update_card(@card.id, card_params)
+    redirect_to board_path(@list.board_id)
+  end
+
+  def destroy
+      @board = Board.find(@list.board_id)
+      @task.destroy
+      redirect_to board_list_path(@board, @list)
+  end
+end
+
+  private
+    def set_card
+      @card = Card.single_card(params[:id])
+    end
+
+    def set_list
+      @list = List.single_list(params[:list_id])
+    end
+
+    def card_params
+      params.require(:card).permit(:title, :description,)
+    end
+
 end
